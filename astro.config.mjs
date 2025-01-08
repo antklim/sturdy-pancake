766e32158af3b603125973a6d616e0f5904b18ec
antklim <antklim@gmail.com>
2025-01-09 07:15:17 +1100

    added ssr config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 // @ts-check
-// @ts-check
 import { defineConfig } from 'astro/config';
+import deno from '@deno/astro-adapter';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    output: 'server',
+    adapter: deno(),
+});
