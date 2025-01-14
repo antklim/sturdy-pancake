9c5c26940762467c9ac1291d0263377a8bb8b5df
antklim <antklim@gmail.com>
2025-01-15 06:27:45 +1100

    added tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 import deno from '@deno/astro-adapter';
+import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-    output: 'server',
     adapter: deno(),
+    output: 'server',
+    integrations: [tailwind()],
 });
